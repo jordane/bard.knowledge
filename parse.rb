a16f25d42efa0afef2c7583f0cc86fad06f64c15
Jordan Evans <evans.jordan.m@gmail.com>
2012-07-28 04:51:20 +0000

    update to parse.rb

--- parse.rb
+++ parse.rb
@@ -3,8 +3,11 @@
 require 'nokogiri'
 require 'json'
 require 'sinatra'
+require '/var/lib/gems/1.8/gems/sinatra-cross_origin-0.2.0/lib/sinatra/cross_origin'
+require '/home/jordane/git/bard.knowledge/parse_classes.rb'
 
 STATS=['Wis', 'Dex', 'Str', 'Con', 'Cha', 'Int']
+CLASSES=['barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'paladin', 'ranger', 'rogue', 'sorcerer', 'wizard']
 
 def slugify (name) 
 name.downcase!
@@ -13,11 +16,41 @@ name.delete!(')')
 name.gsub(' ', '-')
 end
 
+get '/classes/:name' do
+
+output = ""
+enable :cross_origin
+  names = params[:name].split(' ')
+  if names.length == 1 and names[0] == "all"
+    files = []
+    Dir.foreach('/home/jordane/git/bard.knowledge/classes/www.d20pfsrd.com/classes/core-classes') do |file|
+      if not file == "." and not file == ".."
+        files.push(file)
+      end
+    end
+    output << files.to_json
+  else 
+    classes = []
+    names.each do |name|
+      if CLASSES.include?(name)
+        classes.push(parse_class(name))
+      end
+    end
+    if classes.length == 1
+      output << classes[0].to_json
+    elsif classes.length > 1
+      output << classes.to_json
+    return output
+    end
+  end
+end 
 
 get '/skills/:name' do
+
+enable :cross_origin
 skills = []
 
-response['Access-Control'] ="allow <*>"
+response['Access-Control'] ="allow <*>, secure=false"
 html = Nokogiri::HTML(File.open("skills"))
 
 
